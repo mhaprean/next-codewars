9dbeec31e59bac1f7159ae9d7dfc741a0536cf8f
mhaprean <marianhaprean@gmail.com>
2023-04-21 15:50:06 +0300

    add code editor component using react-ace

---
+++ app/components/editor/CodeEditor.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-twilight';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import { useState } from 'react';
+
+
+
+interface IPropsCodeEditor {
+  value: string;
+  onChange: (newValue: string) => void;
+}
+
+const CodeEditor = ({ value, onChange }: IPropsCodeEditor) => {
+
+  const handleChange = (newValue: string) => {
+    console.log('change', newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <AceEditor
+      className="w-full h-full overflow-y-auto"
+      value={value}
+      mode="javascript"
+      theme="twilight"
+      fontSize={16}
+      tabSize={2}
+      onChange={handleChange}
+      name="UNIQUE_ID_OF_DIV"
+      editorProps={{ $blockScrolling: true }}
+      style={{ width: '100%', height: '100%', overflowY: 'auto' }}
+    />
+  );
+};
+
+export default CodeEditor;
--- app/components/editor/KataContent.tsx
+++ app/components/editor/KataContent.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import ReactMarkdown from 'react-markdown';
 import Split from 'react-split';
-import CodeBlock from './CodeBlock';
-import SyntaxHighlighter from 'react-syntax-highlighter';
 import KataInstructions from './KataInstructions';
+import CodeEditor from './CodeEditor';
+import { useState } from 'react';
 
 const KataContent = () => {
   const kataDescription =
@@ -14,6 +13,45 @@ const KataContent = () => {
   <code>I</code>, <code>V</code>, <code>X</code>, <code>L</code>, <code>C</code>, <code>D</code> and <code>M</code>.</p>
   <pre><strong>Symbol</strong>       <strong>Value</strong>I             1V             5X             10L             50C             100D             500M             1000</pre><p>For example,&nbsp;<code>2</code> is written as <code>II</code>&nbsp;in Roman numeral, just two one&#39;s added together. <code>12</code> is written as&nbsp;<code>XII</code>, which is simply <code>X + II</code>. The number <code>27</code> is written as <code>XXVII</code>, which is <code>XX + V + II</code>.</p><p>Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not <code>IIII</code>. Instead, the number four is written as <code>IV</code>. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as <code>IX</code>. There are six instances where subtraction is used:</p><ul>	<li><code>I</code> can be placed before <code>V</code> (5) and <code>X</code> (10) to make 4 and 9.&nbsp;</li>	<li><code>X</code> can be placed before <code>L</code> (50) and <code>C</code> (100) to make 40 and 90.&nbsp;</li>	<li><code>C</code> can be placed before <code>D</code> (500) and <code>M</code> (1000) to make 400 and 900.</li></ul><p>Given an integer, convert it to a roman numeral.</p><p>&nbsp;</p><p><strong class="example">Example 1:</strong></p><pre><strong>Input:</strong> num = 3<strong>Output:</strong> &quot;III&quot;<strong>Explanation:</strong> 3 is represented as 3 ones.</pre><p><strong class="example">Example 2:</strong></p><pre><strong>Input:</strong> num = 58<strong>Output:</strong> &quot;LVIII&quot;<strong>Explanation:</strong> L = 50, V = 5, III = 3.</pre><p><strong class="example">Example 3:</strong></p><pre><strong>Input:</strong> num = 1994<strong>Output:</strong> &quot;MCMXCIV&quot;<strong>Explanation:</strong> M = 1000, CM = 900, XC = 90 and IV = 4.</pre>
   <p>&nbsp;</p><p><strong>Constraints:</strong></p><ul>	<li><code>1 &lt;= num &lt;= 3999</code></li></ul>`;
+
+  const initialCode = `
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+  const arr = [1000, 500, 100,50,10,5,1];
+  const symbols = ['M', 'D','C','L','X','V','I'];
+  let res = '';
+  for(let i=0; i<arr.length; i++){
+      res+=symbols[i].repeat(Math.floor(num/arr[i]));
+      num = num%arr[i];
+      if(i==0 && num>=900){
+          res+='CM';
+          num -=900;
+      }else if(i==1 && num>=400){
+          res+='CD';
+          num-=400;
+      }else if(i==2 && num>=90){
+          res+='XC';
+          num-=90;
+      }else if(i==3 && num>=40){
+          res+='XL';
+          num-=40
+      }else if(i==4 && num>=9){
+          res+='IX';
+          num-=9
+      }else if(i==5 && num>=4){
+          res+='IV';
+          num-=4
+      }
+  }
+  return res;
+};
+`;
+
+  const [code, setCode] = useState(initialCode);
+
   return (
     <div>
       <Split direction="horizontal" className="flex" style={{ height: `calc(100vh - 80px)` }}>
@@ -26,70 +64,7 @@ const KataContent = () => {
         </div>
         <Split className="flex flex-col" direction="vertical">
           <div className="bg-gray-300 dark:bg-gray-800 min-h-[200px] flex-grow overflow-y-auto">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis temporibus fugiat facere dicta autem aliquam, unde rem hic
-              soluta possimus, praesentium, quam quisquam omnis velit consequuntur sed? Consequatur, ipsa.
-            </p>
+            <CodeEditor value={code} onChange={setCode} />
           </div>
           <div className="bg-gray-300 dark:bg-gray-800 min-h-[50px] max-h-[300px] overflow-y-auto">
             <p>
